c4366c08f4515c37c2976ce6f874df79f6b404a3
gauravKataria0420 <gauravkatariya29@gmail.com>
2022-03-08 15:28:06 +0530

    final commit

--- Backend/App.js
+++ Backend/App.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(console.log("connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+app.use("/", (req, res) => {});
+
+app.listen("4000", () => {
+  console.log("Backend is Runing on 4000 Port");
+});
