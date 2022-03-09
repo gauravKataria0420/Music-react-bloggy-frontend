5467cc9d7cdd51886f996d207fb48012524b4267
gauravKataria0420 <gauravkatariya29@gmail.com>
2022-03-09 16:37:44 +0530

    register and login complete in backend

--- Backend/App.js
+++ Backend/App.js
@@ -2,14 +2,19 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./Routes/auth");
 dotenv.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("connected to MongoDB"))
   .catch((err) => console.log(err));
 
-app.use("/", (req, res) => {});
+app.use("/api/auth", authRoute);
+app.use("/", (req, res) => {
+  res.send("hello");
+});
 
 app.listen("4000", () => {
   console.log("Backend is Runing on 4000 Port");
---
+++ Backend/Models/post.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const PostSchema = new mongoose.Schema(
+  {
+    Post: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    decs: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    categories: {
+      type: Array,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", PostSchema);
