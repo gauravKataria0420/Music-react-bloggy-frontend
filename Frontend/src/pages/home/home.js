664733680b1c697a85674c02cbc6e1ce22b6073e
gauravKataria0420 <gauravkatariya29@gmail.com>
2022-03-14 16:23:05 +0530

    backend is completed

--- Frontend/src/pages/home/home.js
+++ Frontend/src/pages/home/home.js
@@ -1,9 +1,47 @@
-import "./home.css";
+import React, { useState, useEffect } from "react";
 import Header from "../../components/Header/header";
 import Posts from "../../components/posts/posts";
 import Sidebar from "../../components/sidebar/sidebar";
+import axios from "axios";
+import "./home.css";
 
 const Home = () => {
+  // const [Post, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      console.log("data getting true....");
+      let url = "http://localhost:4000/api/post";
+      const res = await axios
+        .get(url)
+        .then((responce) => {
+          console.log(responce);
+          console.log("all data get....");
+        })
+        .catch((error) => {
+          console.log(error);
+          console.log("component mount... but don't get data");
+        });
+    };
+
+    fetchPosts();
+    // console.log(fetchPosts());
+  }, []);
+
+  // useEffect(() => {
+  //   fetch("http://localhost:4000/api/post/")
+  //     .then((res) => {
+  //       res.json(res);
+  //       console.log(res);
+  //     })
+  //     .then((post) => {
+  //       setPosts(post);
+  //       console.log(post);
+  //     });
+  // }, []);
+
+  console.log("home running");
+
   return (
     <>
       <Header />
