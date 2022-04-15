4ea5edee05b7a379bda9febdad18bfb166d9da18
gauravKataria0420 <gauravkatariya29@gmail.com>
2022-04-15 16:25:20 +0530

    backend changes done

--- Backend/App.js
+++ Backend/App.js
@@ -4,25 +4,24 @@ const dotenv = require("dotenv");
 const multer = require("multer");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const homeRoute = require("./Routes/Home");
 const authRoute = require("./Routes/auth");
 const userRoute = require("./Routes/users");
 const postRoute = require("./Routes/posts");
 const categoryRoute = require("./Routes/categories");
 const path = require("path");
 dotenv.config();
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+// MiddleWare
 app.use(express.json());
 app.use(cors());
-app.use("/images", express.static(path.join(__dirname, "/images")));
-// MONGO AND MONGOOSE PROCESS
 
+// MONGO AND MONGOOSE PROCESS
 mongoose
   .connect(process.env.MONGO_URL)
   .then(console.log("connected to MongoDB"))
   .catch((err) => console.log(err));
-
-app.get("/", (req, res) => {
-  res.send("Welcome in MusicBlog Backend");
-});
 
 // MULTER PROCESS
 const storage = multer.diskStorage({
@@ -42,12 +41,14 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
 });
 
 // USING ROUTE
+app.use("/", homeRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/post", postRoute);
 app.use("/api/categories", categoryRoute);
 
 // LISTENING SERVER
-app.listen("4000", () => {
-  console.log("Backend is Runing on 4000 Port");
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Backend is Runing on ${port}...`);
 });
